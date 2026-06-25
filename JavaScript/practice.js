75cc50c9d58900ab6c5db96c42bba2f32313111b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-25 14:53:44 +0530

    learning difference between let var and const

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -3,11 +3,25 @@
 // console.log('C');
 
 
-let a = 5; 
+// let a = 5; 
 
-function sayHello() {
-    console.log("Hello");
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+// sayHello();
+// console.log(a);
+
+
+function scope() {
+    if(true){
+        // let num = 5;
+        const num = 5;
+        // console.log(num);  // it will print 5
+    }
+    // console.log(num);  // using let it would give you reference error : num is not defined
+    console.log(num);  // using const it would give you reference error : num is not defined
 }
 
-sayHello();
-console.log(a);
+scope();
+
