1ca081796c01652b8c20510f2d4cec27ef753c10
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-25 15:29:52 +0530

    hoisting

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -13,17 +13,27 @@
 // console.log(a);
 
 
-function scope() {
-    if(true){
-        // let num = 5;
-        // const num = 5;
-        var num = 5;
-        // console.log(num);  // it will print 5
-    }
-    // console.log(num);  // using let it would give you reference error : num is not defined
-    // console.log(num);  // using const it would give you reference error : num is not defined
-    console.log(num);  // using var it will print num.
-}
+// function scope() {
+//     if(true){
+//         // let num = 5;
+//         // const num = 5;
+//         var num = 5;
+//         // console.log(num);  // it will print 5
+//     }
+//     // console.log(num);  // using let it would give you reference error : num is not defined
+//     // console.log(num);  // using const it would give you reference error : num is not defined
+//     // console.log(num);  // using var it will print num.
+// }
+// console.log(num);  // using let const and var it will not print num.
 
-scope();
+// scope();
 
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let num1 = 1;
+var num2 = 3;
+const num3 = 5;
