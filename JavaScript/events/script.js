6330ce3b93623e3c98d7495cc6129464ad33fc9d
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-29 16:57:42 +0530

    events

---
+++ JavaScript/events/script.js
@@ -0,0 +1,7 @@
+const grandparent = document.querySelector("#grandparaent");
+const parent = document.querySelector("#paraent");
+const child = document.querySelector("#child");
+
+child.addEventListener('click',()=>{
+    console.log("child is clicked");
+})
