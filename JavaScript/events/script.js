41a64e48aab56b54dc159afdcf038b2015c7608b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-30 14:47:24 +0530

    event bubbling and event capturing

--- JavaScript/events/script.js
+++ JavaScript/events/script.js
@@ -1,6 +1,7 @@
 const grandparent = document.querySelector("#grandparent");
 const parent = document.querySelector("#parent");
 const child = document.querySelector("#child");
+const body = document.querySelector("body");
 
 // Capturing phase (top -> down)
 grandparent.addEventListener("click", () => {
@@ -9,13 +10,20 @@ grandparent.addEventListener("click", () => {
 
 parent.addEventListener("click", () => {
     console.log("capturing: parent");
+
 }, true);
 
 child.addEventListener("click", () => {
     console.log("capturing: child");
 }, true);
 
+body.addEventListener("click", () =>{
+    console.log("capturing body")
+}, true)
+
+
 // Bubbling phase (target -> up)
+
 child.addEventListener("click", () => {
     console.log("bubbling: child");
 });
@@ -26,4 +34,9 @@ parent.addEventListener("click", () => {
 
 grandparent.addEventListener("click", () => {
     console.log("bubbling: grandparent");
+
 });
+
+body.addEventListener("click", () =>{
+    console.log("bubbling body")
+},)
