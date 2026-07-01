3ac6761230356f08f32551c284f9fc8aabb0db4e
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-01 16:42:33 +0530

    callback hell

--- JavaScript/callback/callback.js
+++ JavaScript/callback/callback.js
@@ -74,43 +74,105 @@
 
 ///////////////////////////////////////////////////////////////////
 
-function login(callack) {
-  console.log("Logging in....");
+// function login(callack) {
+//   console.log("Logging in....");
 
-  setTimeout(() => {
-    console.log("logged in successfully");
-    callack();
-  }, 5000);
+//   setTimeout(() => {
+//     console.log("logged in successfully");
+//     callack();
+//   }, 5000);
+// }
+
+// function getUser(callack) {
+//   console.log("getting user....");
+//   setTimeout(() => {
+//     console.log("User details loaded");
+//     callack();
+//   }, 2000);
+// }
+
+// function getOrders(callack) {
+//   console.log("Getting orders");
+
+//   setTimeout(() => {
+//     console.log("Orders loaded");
+//     callack();
+//   }, 1000);
+// }
+
+// function getOrderDetails() {
+//   console.log("Getting order details.....");
+//   setTimeout(() => {
+//     console.log("Order details loaded");
+//   }, 4000);
+// }
+
+// login(() => {
+//   getUser(() => {
+//     getOrders(() => {
+//       getOrderDetails();
+//     });
+//   });
+// });
+
+//////////////////////////////callback hell////////////////////////////////////////////
+
+function one(callack) {
+  console.log("1");
+  callack();
+}
+function two(callack) {
+  console.log("2");
+  callack();
+}
+function three(callack) {
+  console.log("3");
+  callack();
+}
+function four(callack) {
+  console.log("4");
+  callack();
+}
+function five(callack) {
+  console.log("5");
+  callack();
+}
+function six(callack) {
+  console.log("6");
+  callack();
+}
+function seven(callack) {
+  console.log("7");
+  callack();
+}
+function eight(callack) {
+  console.log("8");
+  callack();
+}
+function nine(callack) {
+  console.log("9");
+  callack();
+}
+function ten(callack) {
+  console.log("10");
 }
 
-function getUser(callack) {
-  console.log("getting user....");
-  setTimeout(() => {
-    console.log("User details loaded");
-    callack();
-  }, 2000);
-}
-
-function getOrders(callack) {
-  console.log("Getting orders");
-
-  setTimeout(() => {
-    console.log("Orders loaded");
-    callack();
-  }, 1000);
-}
-
-function getOrderDetails() {
-  console.log("Getting order details.....");
-  setTimeout(() => {
-    console.log("Order details loaded");
-  }, 4000);
-}
-
-login(() => {
-  getUser(() => {
-    getOrders(() => {
-      getOrderDetails();
+one(() => {
+  two(() => {
+    three(() => {
+      four(() => {
+        five(() => {
+          six(() => {
+            seven(() => {
+              eight(() => {
+                nine(() => {
+                  ten();
+                });
+              });
+            });
+          });
+        });
+      });
     });
   });
 });
