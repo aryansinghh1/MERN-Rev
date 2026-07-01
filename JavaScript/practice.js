3ff8b2e61923a0409f54169e8b814455c426ec90
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-01 15:29:29 +0530

    learning callback function

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -71,12 +71,87 @@
 
 /////////////////////Promise//////////////////////////////////
 
-console.log("1");
-Promise.resolve().then(()=>{console.log("9")})
-console.log("2");
-setTimeout(()=>console.log("3"),3000)
-console.log("4");
-setTimeout(()=>console.log("5"),1000)
-Promise.resolve().then(()=>{console.log("8")})
-console.log("6");
-setTimeout(()=>console.log("7"),2000)
+// console.log("1");
+// Promise.resolve().then(()=>{console.log("9")})
+// console.log("2");
+// setTimeout(()=>console.log("3"),3000)
+// console.log("4");
+// setTimeout(()=>console.log("5"),1000)
+// Promise.resolve().then(()=>{console.log("8")})
+// console.log("6");
+// setTimeout(()=>console.log("7"),2000)
+
+
+/////////////////////////////////callack///////////////////////
+function one(){
+    console.log("This is one");
+}
+
+function two(fn){
+ fn();
+}
+
+two(one);
+
+///////////////////////////////////////////////
+
+// function greet (name){
+//     console.log(`hello ${name}`);
+// }
+
+// function callGreet(callback){
+//     console.log("start");
+//     // callback("aryan");
+//     callback();
+//     console.log("end");
+// }
+
+// callGreet(greet);
+// callGreet(greet("jerry"));   //we can't do this
+
+// callGreet(()=>{
+//     greet("aryan");
+// })
+
+////////////////////////////////////////////////////
+
+// setTimeout(()=>{
+// console.log("hello");
+// },1000)
+
+////////////////////////////////////////////////
+
+// function login(){
+//     console.log("Logging in....");
+
+//     setTimeout(() => {
+//         console.log("logged in successfully");
+//     }, 5000);
+// }
+
+// function getUser(){
+//     console.log("getting user....");
+//     setTimeout(() => {
+//         console.log("User details loaded");
+//     }, 2000);
+// }
+
+// function getOrders(){
+//     console.log("getting orders");
+
+//     setTimeout(() => {
+//         console.log("Orders loaded");
+//     }, 1000);
+// }
+
+// function getOrderDetails(){
+//     console.log("Getting order details.....");
+//     setTimeout(() => {
+//         console.log("Order details loaded");
+//     }, 4000);
+// }
+
+// login();
+// getUser();
+// getOrders();
+// getOrderDetails();
