5c6a0d76040ea2d91f5fd6c48cc9300ec442d612
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-02 14:42:58 +0530

    promise code

--- JavaScript/Promise/Promises.js
+++ JavaScript/Promise/Promises.js
@@ -1,3 +1,23 @@
 const promise  = new Promise((resolve,reject) =>{
+    let success = false;
 
+    setTimeout(()=>{
+        if(success){
+            resolve("Success");
+        }
+        else{
+            reject("Failed")
+
+        }
+    },1000)
+});
+
+promise.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
+.finally(()=>{
+    console.log("Promies complete");
 })
