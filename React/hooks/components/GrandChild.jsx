78cd857431f5da9b289141587e485840d3246449
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-10 15:37:20 +0530

    usecontext

--- React/hooks/components/GrandChild.jsx
+++ React/hooks/components/GrandChild.jsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../src/context/userContext";
 
 function GrandChild(){
-    const {name} = useContext(UserContext)
+    const {name, setName} = useContext(UserContext);
+    const [user,setUser] = useState('');
     return(
 
-        <div>
+        <div  style={{border: "1px green solid", padding :'10px'}}>
         <p>This is GrandChild</p>
         <p>Good Morning: {name}</p>
+        <input
+            type="text"
+            placeholder="Enter User name"
+            value={user}
+            onChange={e => setUser(e.target.value)}
+        />
+        <button onClick={() => setName(user)}>Update Name</button>
         </div>
     )
     
--- React/hooks/components/Parent.jsx
+++ React/hooks/components/Parent.jsx
@@ -1,9 +1,10 @@
-import Child from "./child";
+import Child from "./Child";
+
 function Parent({name}){
     return(
 
         <div style={{border: '1px solid blue', padding: '10px'}}>
-        <p>This is Parent</p>
+        <p>This is Parent : {name}</p>
         {/* Parent keeps drilling the prop down to Child. */}
         <Child name={name}/>
         </div>
