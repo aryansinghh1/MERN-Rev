a348a5e925d97a380e8446ecd905936b52f42f7b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-10 16:46:44 +0530

    routing

--- React/React-App/src/App.jsx
+++ React/React-App/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import About from "./Pages/About/About";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
