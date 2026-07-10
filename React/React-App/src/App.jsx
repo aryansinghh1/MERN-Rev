3d3ace47fdf1fe780e1d8d318a06e338ef2c2f6a
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-10 16:47:58 +0530

    adding navbar and footer

--- React/React-App/src/App.jsx
+++ React/React-App/src/App.jsx
@@ -5,16 +5,20 @@ import Dashboard from "./Pages/Dashboard/Dashboard";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import About from "./Pages/About/About";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
