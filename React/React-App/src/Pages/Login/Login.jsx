61293769a45beff7280b8b3d24671a87052cf5c0
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-10 16:01:45 +0530

    folder Structure

---
+++ React/React-App/src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+function Login(){
+    return(
+        <div>
+            <form action="">
+            <input type="email" placeholder="Enter Your Email" />
+            <input type="password" placeholder="Enter Your Password"/>
+            <button type="submit">Login</button>
+            </form>
+
+        </div>
+    )
+}
+export default Login;
