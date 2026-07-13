57260f28301441393216b744b9dc527a46a34336
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-13 16:42:03 +0530

    creating theme context

--- React/React-App/src/Components/Navbar/Navbar.jsx
+++ React/React-App/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-function Navbar(){
-    return(
-        <div>
-            <p>This is Navbar</p>
-        </div>
-    )
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+
+function Navbar() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <div className={theme}>
+      <p>This is Navbar</p>
+      Theme: {theme}
+      <button onClick={() => toggleTheme}>Toggle Theme</button>
+    </div>
+  );
 }
 export default Navbar;
--- React/React-App/src/Pages/Dashboard/Dashboard.jsx
+++ React/React-App/src/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Courses from '../../data/Course';
 import CourseCard from '../../Components/CourseCard/CourseCard';
+import { ThemeContext } from '../../context/ThemeContext';
 
 function Dashboard() {
+    const{theme} = useContext(ThemeContext);
     return (
-        <div>
+
+        <div className={theme}>
             <h2>Dashboard</h2>
             <h3>Courses</h3>
+            
             <div className="courses-grid">
                 {Courses.map(course => (
                     <CourseCard key={course.id} {...course} />
