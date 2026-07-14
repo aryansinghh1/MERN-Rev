572bf12939d04fe5080bcc55aceeb28605f10c72
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-14 16:51:48 +0530

    routing

--- NodeJs/http/http.js
+++ NodeJs/http/http.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    
+
   //   res.end("response from http server");
 
   if (req.url === "/") {
@@ -9,6 +9,12 @@ const server = http.createServer((req, res) => {
   }
   else if(req.url === "/about"){
     res.end("This is about page");
+  }
+  else if(req.url === "/login"){
+    res.end("This is Login page");
+  }
+  else if(req.url === "/signup"){
+    res.end("This is Signup page");
   }
 
 });
