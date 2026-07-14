faf8af8cc8776f10b296a8ca1a96d7c3acdc6f22
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-14 14:44:25 +0530

    adding dark and light mode

--- React/React-App/src/Pages/Login/Login.jsx
+++ React/React-App/src/Pages/Login/Login.jsx
@@ -1,16 +1,32 @@
 import "./Login.css";
+import { useContext } from "react";
+import { ThemeContext } from '../../context/ThemeContext';
 
-function Login(){
-    return(
-        <div className="login-container">
-            <form action="">
-            <h2>Welcome Back</h2>
-            <input className="input-box" type="email" placeholder="Enter Your Email" required/>
-            <input className="input-box" type="password" placeholder="Enter Your Password" required/>
-            <button className="sub-btn" type="submit">Login</button>
-            </form>
-
-        </div>
-    )
+function Login() {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div className={theme}>
+      <div className="login-container">
+        <form action="">
+          <h2>Welcome Back</h2>
+          <input
+            className="input-box"
+            type="email"
+            placeholder="Enter Your Email"
+            required
+          />
+          <input
+            className="input-box"
+            type="password"
+            placeholder="Enter Your Password"
+            required
+          />
+          <button className="sub-btn" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default Login;
--- React/React-App/src/Pages/Signup/Signup.jsx
+++ React/React-App/src/Pages/Signup/Signup.jsx
@@ -1,7 +1,11 @@
 import "../Signup/Signup.css";
+import { useContext } from "react";
+import { ThemeContext } from '../../context/ThemeContext';
 
 function Signup(){
+    const { theme } = useContext(ThemeContext);
     return(
+        <div className={theme}>
         <div className="signup-container">
             <form action="">
             <h2>Create Account</h2>
@@ -10,6 +14,7 @@ function Signup(){
             <input className="input-box" type="password" placeholder="Create Your Password" required/>
             <button className="sub-btn" type="submit">Sign Up</button>
             </form>
+        </div>
         </div>
     )
 }
