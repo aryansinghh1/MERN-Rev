ab333a45be9cfb021475d1d2d304e3d62c8eae00
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-14 14:32:04 +0530

    creating signup page

--- React/React-App/src/Pages/Login/Login.jsx
+++ React/React-App/src/Pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import "./Login.css";
 function Login(){
     return(
         <div className="login-container">
-            <form  action="">
+            <form action="">
             <h2>Welcome Back</h2>
             <input className="input-box" type="email" placeholder="Enter Your Email" required/>
             <input className="input-box" type="password" placeholder="Enter Your Password" required/>
--- React/React-App/src/Pages/Signup/Signup.jsx
+++ React/React-App/src/Pages/Signup/Signup.jsx
@@ -1,7 +1,15 @@
+import "../Signup/Signup.css";
+
 function Signup(){
     return(
-        <div>
-            <p>This is Signup</p>
+        <div className="signup-container">
+            <form action="">
+            <h2>Create Account</h2>
+            <input className="input-box" type="text" placeholder="Enter Your Name" required/>
+            <input className="input-box" type="email" placeholder="Enter Your Email" required/>
+            <input className="input-box" type="password" placeholder="Create Your Password" required/>
+            <button className="sub-btn" type="submit">Sign Up</button>
+            </form>
         </div>
     )
 }
