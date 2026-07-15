7f3c747c47a27bfce98f4e678a653910433c43b1
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-15 16:41:57 +0530

    posting student data

--- Express/server.js
+++ Express/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());  //to post data students
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -11,6 +13,7 @@ app.get("/login", (req, res) => {
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
 // app.post("/login",(req,res)=>{
 //     res.send({success:true,message:"logged in"});
 // })
@@ -24,6 +27,7 @@ app.get("/login", (req, res) => {
 // })
 
 ///////////////////////////////////route using id//////////////////////////////////////////////////////
+
 // const students = [
 //   { id: 1, name: "tom", age: 22 },
 //   { id: 2, name: "tom2", age: 24 },
@@ -41,17 +45,36 @@ app.get("/login", (req, res) => {
 //   res.send(`Your id is ${id}`);
 // });
 
-////////////////////////////////////////route using course////////////////////////////////////////////////////
+////////////////////////////////////////route using name & course////////////////////////////////////////////////////
+
+// const students = [
+//   { id: 1, name: "tom", age: 22 ,course: "mern" },
+//   { id: 2, name: "tom", age: 24,course: "cpp" },
+//   { id: 3, name: "harry", age: 32 ,course: "dsa"},
+// ];
+
+// app.get("/student/name/:name/course/:course", (req, res) => {
+// const course = req.params.course;
+// const name = req.params.name;
+// const student = students.find((s) => s.name === name && s.course === course);
+// res.json({ success: true, data: student });
+// });
+
+/////////////////////////////////////////////post students name//////////////////////////////////////////////////
+
 const students = [
-  { id: 1, name: "tom", age: 22 ,course: "MERN" },
-  { id: 2, name: "tom", age: 24,course: "cpp" },
-  { id: 3, name: "harry", age: 32 ,course: "dsa"},
+  { id: 1, name: "tom", age: 22, course: "mern" },
+  { id: 2, name: "tom", age: 24, course: "cpp" },
+  { id: 3, name: "harry", age: 32, course: "dsa" },
 ];
 
-app.get("/student/course/:course", (req, res) => {
-const course = req.params.course;
-const student = students.find((s) => s.course === course);
-res.json({ success: true, data: student });
-});
+app.post("/student", (req, res) => {
+  const { name, age, course } = req.body;
+  const newStudent = { id: students.length + 1, name, age, course };
+  students.push(newStudent);
+  res.status(201).json({ success: true, data: newStudent });
+}); 
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(PORT, () => console.log("Server is running"));
