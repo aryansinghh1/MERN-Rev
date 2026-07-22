8e347a52a75deb63285751a6b03570f37db7b374
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-22 15:12:33 +0530

    creating mongoose Schema

--- NodeJs/simpleApp/src/models/Course.js
+++ NodeJs/simpleApp/src/models/Course.js
@@ -1,31 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: true,
-        
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
     instructor: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     duration: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    level:{
-        type: String,
-        enum:['Beginner','Intermediate','Advance'],
-        required: true,
+    level: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advance"],
+      required: true,
     },
     imageUrl: {
-        type: String,
-        required: true,
-    }
-
-})
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export default mongoose.model("Course",courseSchema);
--- NodeJs/simpleApp/src/models/User.js
+++ NodeJs/simpleApp/src/models/User.js
@@ -1,34 +1,40 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    emum: ["Student", "Instructor", "Admin"],
-    default: "student",
-  },
-  courses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      emum: ["Student", "Instructor", "Admin"],
+      default: "student",
+    },
+    courses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+export default mongoose.model("User",userSchema);
