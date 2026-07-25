41316d06536770444f22cf3d8453246648802fff
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-25 12:11:19 +0530

    finding max element

--- practice.js
+++ practice.js
@@ -15,6 +15,31 @@
 
 // console.log(ans);
 
-let name = "Aryan";
+// let name = "Aryan";
 
-console.log(`Hello ${name}`);
+// console.log(`Hello ${name}`);
+
+//////////////////////////////////////////////////////////
+
+//Find max element in arr
+
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// console.log(findMax(arr));
+console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8]));
+
+
+
+
+
