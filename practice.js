955e78e5a8908c2ba13896413639d88eab6cc1b9
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-25 12:19:42 +0530

    frequency of elements

--- practice.js
+++ practice.js
@@ -1,5 +1,7 @@
 // let arr = [1,2]
 
+import { constrainedMemory } from "node:process";
+
 // arr.push(3);
 // arr.push(4);
 // arr.shift();
@@ -23,23 +25,59 @@
 
 //Find max element in arr
 
-function findMax(arr) {
-  let max = arr[0];
+// function findMax(arr) {
+//   let max = arr[0];
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (max < arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
 
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// console.log(findMax(arr));
-console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8]));
+// // console.log(findMax(arr));
+// console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8]));
+
+//////////////////////////////////////////////////////////////////
+
+//Reverse an array
+
+// function reverseArray(arr) {
+//   let start = 0;
+//   let end = arr.length - 1;
+
+//   while (start < end) {
+//     let temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+
+//     start++;
+//     end--;
+//   }
+
+//   return arr;
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// console.log(reverseArray(arr));
 
 
+let arr = [1,2,2,3,1];
 
+let freq = {};
 
+for(let num of arr){
 
+    if(freq[num]){
+        freq[num]++;
+    }else{
+        freq[num] = 1;
+    }
+
+}
+
+console.log(freq);
